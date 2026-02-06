77b5a0f292e079c3b9b069282d3b18ebb6a06023
Purushottam Jha <j.purushottam@iitg.ac.in>
2026-02-06 18:48:22 +0530

    Update Vehicle.h

--- ALNS/Vehicle.h
+++ ALNS/Vehicle.h
@@ -2,7 +2,7 @@
 #include <string>
 
 struct Vehicle {
-    int id; // Internal index
+    int id;
     std::string originalId;
     std::string fuelType;
     std::string mode;
@@ -10,7 +10,8 @@ struct Vehicle {
     double costPerKm;
     double speed;
     double x, y;
-    double startTime; // In minutes from midnight
-    double endTime;   // In minutes from midnight (default to end of day)
+    double startTime; 
+    double endTime; 
     bool premium;
 };
+
