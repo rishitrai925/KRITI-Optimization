a3f4365caf8928c9d33088b48d3794958efecb77
JunoSat <sathvik.avenger@gmail.com>
2026-03-07 22:01:54 +0530

    Rename destroy functions for clarity

--- ALNS/DestroyOperators.h
+++ ALNS/DestroyOperators.h
@@ -6,10 +6,10 @@
 #include "Vehicle.h"
 
 void randomDestroy(std::vector<Route>&, int q);
-void worstCostDestroy(std::vector<Route>&, int q);
+void LongestRouteTailRemoval(std::vector<Route>&, int q);
 void vehicleDestroy(std::vector<Route>&);
 #include "CSVReader.h"
-void timeWindowDestroy(std::vector<Route>&,
+void WorstRouteTailRemoval(std::vector<Route>&,
                        const std::vector<Employee>&,
                        const std::vector<Vehicle>&,
                        const Metadata&);
